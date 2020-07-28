19d4740e0e5c79ff5f0ea2883757e3de610a98c8
George <g.popov44@icloud.com>
2020-07-28 22:46:50 +0300

    +testing

---
+++ 17_advancedTesting/1_errorsTesting/read.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+/* import getFunction from '../functions.js';
+
+const read = getFunction(); */
+
+// BEGIN (write your solution here)
+/* test('test!', () => {
+  expect(() => {
+    read('/undefined');
+  }).toThrow("ENOENT: no such file or directory, open '/undefined'");
+  expect(() => {
+    read('/etc');
+  }).toThrow("EISDIR: illegal operation on a directory, read");
+
+}); */
+// END
