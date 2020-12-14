1bc865602770403318689e9a369196479c072c9e
George <g.popov44@icloud.com>
2020-12-14 18:09:29 +0300

    commit hexlet

--- 20_Redux/10_Forms/index.jsx
+++ 20_Redux/10_Forms/index.jsx
@@ -5,16 +5,11 @@ import { createStore } from 'redux';
 import reducers from './reducers/index.js';
 import App from './components/App.jsx';
 
-/* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
-/* eslint-enable */
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('container'),
-);
+export default (gon) => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('container'),
+  );
+}
